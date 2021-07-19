4c63150ff7610a8291698c3c1c327a9c5a9706b1
VadymBatryn <vadimbatrin4@gmail.com>
2021-07-19 18:13:59 +0300

    feat: set  and saving todos

--- src/store/reducer.js
+++ src/store/reducer.js
@@ -1,5 +1,6 @@
 import { createSlice } from '@reduxjs/toolkit';
-
+import { State } from 'react-native-gesture-handler';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 const todosSlice = createSlice({
 	name: 'todos',
 	initialState: {
@@ -9,11 +10,23 @@ const todosSlice = createSlice({
 		addTodo(state, action) {
 			state.todos.push(action.payload);
 		},
+		setTodos(state, action) {
+			state.todos = action.payload;
+		},
 		removeTodo(state, action) {
 			state.todos = state.todos.filter((todo) => todo.id !== action.payload);
+		},
+		saveToLocal(state) {
+			const localTodos = JSON.stringify(state.todos);
+			try {
+				AsyncStorage.setItem('todos', localTodos);
+			} catch (err) {
+				console.log(err);
+			}
 		},
 	},
 });
 export default todosSlice.reducer;
 
-export const { addTodo, removeTodo } = todosSlice.actions;
+export const { addTodo, removeTodo, setTodos, saveToLocal } =
+	todosSlice.actions;
