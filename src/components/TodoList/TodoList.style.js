057928b463e3e62c1e4835ac932e1c8cab463faa
VadymBatryn <vadimbatrin4@gmail.com>
2021-08-06 02:34:53 +0300

    feat: add todoList component styles

---
+++ src/components/TodoList/TodoList.style.js
@@ -0,0 +1,16 @@
+import { StyleSheet } from 'react-native';
+export default StyleSheet.create({
+	avoidingView: {
+		flex: 1,
+	},
+	title: {
+		fontSize: 28,
+		fontWeight: 'bold',
+		marginLeft: 30,
+		marginVertical: 30,
+	},
+	tasksContainer: {
+		padding: 10,
+		alignItems: 'center',
+	},
+});
