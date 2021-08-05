b3e2a086500d524c413dde9c7d64dff07942c96f
VadymBatryn <vadimbatrin4@gmail.com>
2021-08-06 02:35:47 +0300

    feat: add CustomButton component styles

---
+++ src/components/CustomButton/CustomButton.style.js
@@ -0,0 +1,11 @@
+import { StyleSheet } from 'react-native';
+export default StyleSheet.create({
+	addTaskButton: {
+		height: 50,
+		width: 50,
+		borderRadius: 25,
+		backgroundColor: '#fff',
+		justifyContent: 'center',
+		alignItems: 'center',
+	},
+});
