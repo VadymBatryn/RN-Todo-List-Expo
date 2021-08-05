03507586d5042528e83d69513129445e21f36187
VadymBatryn <vadimbatrin4@gmail.com>
2021-08-06 02:35:28 +0300

    feat: add Input component styles

---
+++ src/components/Input/Input.style.js
@@ -0,0 +1,17 @@
+import { StyleSheet } from 'react-native';
+export default StyleSheet.create({
+	addTaskContainer: {
+		height: 100,
+		padding: 20,
+		flexDirection: 'row',
+		justifyContent: 'space-between',
+		alignItems: 'center',
+	},
+	taskInput: {
+		width: '80%',
+		height: '80%',
+		borderRadius: 40,
+		backgroundColor: '#fff',
+		paddingHorizontal: 20,
+	},
+});
